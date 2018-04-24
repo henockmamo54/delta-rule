ad8a88bb5e8aa9ebc5a1cb6bb1f08e4412af6aea
DESKTOP-IUH4BPV\Henock <henockmamo54@gmail.com>
2018-04-24 11:36:08 +0900

    project created

---
+++ delta rule/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btn_browse = new System.Windows.Forms.Button();
+            this.txtfilelocation = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.lbl_w2 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lbl_w1 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.lbl_lr = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbl_theta = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbl_iteration = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea5.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea5);
+            legend5.Enabled = false;
+            legend5.Name = "Legend1";
+            this.chart1.Legends.Add(legend5);
+            this.chart1.Location = new System.Drawing.Point(6, 19);
+            this.chart1.Name = "chart1";
+            series5.ChartArea = "ChartArea1";
+            series5.Legend = "Legend1";
+            series5.Name = "Series1";
+            this.chart1.Series.Add(series5);
+            this.chart1.Size = new System.Drawing.Size(552, 330);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.chart1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 52);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(564, 359);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Dataset Plot";
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(484, 13);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(75, 23);
+            this.btn_start.TabIndex = 3;
+            this.btn_start.Text = "Start";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btn_browse);
+            this.groupBox2.Controls.Add(this.btn_start);
+            this.groupBox2.Controls.Add(this.txtfilelocation);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(564, 53);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            // 
+            // btn_browse
+            // 
+            this.btn_browse.Location = new System.Drawing.Point(405, 11);
+            this.btn_browse.Name = "btn_browse";
+            this.btn_browse.Size = new System.Drawing.Size(75, 23);
+            this.btn_browse.TabIndex = 2;
+            this.btn_browse.Text = "Browse";
+            this.btn_browse.UseVisualStyleBackColor = true;
+            this.btn_browse.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtfilelocation
+            // 
+            this.txtfilelocation.Location = new System.Drawing.Point(62, 13);
+            this.txtfilelocation.Name = "txtfilelocation";
+            this.txtfilelocation.Size = new System.Drawing.Size(337, 20);
+            this.txtfilelocation.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Location";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApplication1.Properties.Resources.Picture2;
+            this.pictureBox1.Location = new System.Drawing.Point(6, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(369, 193);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.pictureBox1);
+            this.groupBox3.Location = new System.Drawing.Point(585, 23);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(382, 218);
+            this.groupBox3.TabIndex = 16;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Perceptron";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.lbl_w2);
+            this.groupBox4.Controls.Add(this.label8);
+            this.groupBox4.Controls.Add(this.lbl_w1);
+            this.groupBox4.Controls.Add(this.label10);
+            this.groupBox4.Controls.Add(this.lbl_lr);
+            this.groupBox4.Controls.Add(this.label5);
+            this.groupBox4.Controls.Add(this.lbl_theta);
+            this.groupBox4.Controls.Add(this.label4);
+            this.groupBox4.Controls.Add(this.lbl_iteration);
+            this.groupBox4.Controls.Add(this.label2);
+            this.groupBox4.Location = new System.Drawing.Point(591, 248);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(369, 163);
+            this.groupBox4.TabIndex = 17;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Parameters";
+            // 
+            // lbl_w2
+            // 
+            this.lbl_w2.AutoSize = true;
+            this.lbl_w2.Location = new System.Drawing.Point(241, 52);
+            this.lbl_w2.Name = "lbl_w2";
+            this.lbl_w2.Size = new System.Drawing.Size(13, 13);
+            this.lbl_w2.TabIndex = 25;
+            this.lbl_w2.Text = "0";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(196, 52);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(21, 13);
+            this.label8.TabIndex = 24;
+            this.label8.Text = "w2";
+            // 
+            // lbl_w1
+            // 
+            this.lbl_w1.AutoSize = true;
+            this.lbl_w1.Location = new System.Drawing.Point(241, 28);
+            this.lbl_w1.Name = "lbl_w1";
+            this.lbl_w1.Size = new System.Drawing.Size(13, 13);
+            this.lbl_w1.TabIndex = 23;
+            this.lbl_w1.Text = "0";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(196, 28);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(21, 13);
+            this.label10.TabIndex = 22;
+            this.label10.Text = "w1";
+            // 
+            // lbl_lr
+            // 
+            this.lbl_lr.AutoSize = true;
+            this.lbl_lr.Location = new System.Drawing.Point(82, 78);
+            this.lbl_lr.Name = "lbl_lr";
+            this.lbl_lr.Size = new System.Drawing.Size(13, 13);
+            this.lbl_lr.TabIndex = 21;
+            this.lbl_lr.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 78);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 13);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Learning Rate";
+            // 
+            // lbl_theta
+            // 
+            this.lbl_theta.AutoSize = true;
+            this.lbl_theta.Location = new System.Drawing.Point(82, 52);
+            this.lbl_theta.Name = "lbl_theta";
+            this.lbl_theta.Size = new System.Drawing.Size(13, 13);
+            this.lbl_theta.TabIndex = 19;
+            this.lbl_theta.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 52);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Theta";
+            // 
+            // lbl_iteration
+            // 
+            this.lbl_iteration.AutoSize = true;
+            this.lbl_iteration.Location = new System.Drawing.Point(82, 28);
+            this.lbl_iteration.Name = "lbl_iteration";
+            this.lbl_iteration.Size = new System.Drawing.Size(13, 13);
+            this.lbl_iteration.TabIndex = 17;
+            this.lbl_iteration.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "# iteration";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(972, 419);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btn_browse;
+        private System.Windows.Forms.TextBox txtfilelocation;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label lbl_w2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lbl_w1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label lbl_lr;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbl_theta;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbl_iteration;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ delta rule/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        static double learningRate = 0.1;
+        List<double[]> data;
+        Chart mychart;
+        List<double> slope = new List<double>();
+        List<double> bias = new List<double>();
+        List<double> errorValues = new List<double>();
+        List<double> weights = new List<double>();
+        List<double[]> weightsHistory = new List<double[]>();
+        int chart_index = 0;
+        double theta = 0.55;
+        int numItteration = 100;
+        bool isChartUpdateEnabled = false;
+        int chartstepSize = 10;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //read parameters
+            readParameters();
+            mychart = this.chart1;
+
+            //read data
+            data = readFile();
+            instantiateChart();
+
+            // apply delta rule
+            deltaRule(data);
+
+            Timer timer1 = new Timer();
+            timer1.Interval = 120;
+            timer1.Enabled = true;
+            timer1.Tick += new System.EventHandler(OnTimerEvent);
+        }
+
+        private void instantiateChart()
+        {
+            try
+            {
+
+                mychart.Series.Add("Series2");
+                mychart.Series.Add("Series3");
+            }
+            catch (Exception e) { }
+
+            mychart.ChartAreas[0].AxisY.Maximum = 6;
+            mychart.ChartAreas[0].AxisY.Minimum = -4;
+            mychart.ChartAreas[0].AxisX.Maximum = 4;
+            mychart.ChartAreas[0].AxisX.Minimum = -1;
+            chart1.ChartAreas[0].AxisX.MajorGrid.LineWidth = 0;
+            chart1.ChartAreas[0].AxisY.MajorGrid.LineWidth = 0;
+            mychart.Series["Series1"].ChartType = SeriesChartType.Point;
+            mychart.Series["Series3"].ChartType = SeriesChartType.Point;
+            mychart.Series["Series2"].ChartType = SeriesChartType.Line;
+            mychart.Series["Series2"].BorderWidth = 2;
+
+            // for scater plot
+            List<double> x_col_c1 = new List<double>();
+            List<double> y_col_c1 = new List<double>();
+            List<double> x_col_c2 = new List<double>();
+            List<double> y_col_c2 = new List<double>();
+
+            for (int i = 0; i < data.Count; i++)
+            {
+                if (data[i][2] == 0)
+                {
+                    x_col_c1.Add(data[i][0]);
+                    y_col_c1.Add(data[i][1]);
+                }
+                else if (data[i][2] == 1)
+                {
+                    x_col_c2.Add(data[i][0]);
+                    y_col_c2.Add(data[i][1]);
+                }
+
+            }
+
+            mychart.Series["Series1"].Points.DataBindXY(x_col_c1, y_col_c1);
+            mychart.Series["Series3"].Points.DataBindXY(x_col_c2, y_col_c2);
+            this.Refresh();
+
+        }
+
+        private void OnTimerEvent(object sender, EventArgs e)
+        {
+            //drawLine(wight 1, weight2);
+            if (chart_index < slope.Count & isChartUpdateEnabled)
+            {
+                drawLine(slope[chart_index], bias[chart_index]);
+
+                lbl_w1.Text = weightsHistory[chart_index][0].ToString();
+                lbl_w2.Text = weightsHistory[chart_index][1].ToString();
+                chart_index = chart_index + chartstepSize;
+            }
+
+        }
+
+        public static List<double[]> readFile(string fileLocation = "data.csv")
+        {
+
+            List<double[]> listA = new List<double[]>();
+
+            using (var reader = new StreamReader(fileLocation))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+                    listA.Add(Array.ConvertAll(values, Double.Parse));
+                }
+            }
+
+            return listA;
+
+        }
+
+        void deltaRule(List<double[]> data)
+        {
+            btn_browse.Enabled = false;
+            btn_start.Enabled = false;
+            slope = new List<double>();
+            bias = new List<double>();
+            errorValues = new List<double>();
+            weights = new List<double>();
+            weightsHistory = new List<double[]>();
+
+            //get number of rows and columns
+            int dataRowCount = data.Count();
+            int dataColumnCount = data[0].Length;
+            if (dataRowCount > 100) chartstepSize = 100;
+            else chartstepSize = 10;
+
+            //initialize random weight for all features
+            Random randomNumGenerator = new Random();
+
+            for (int c = 0; c < dataColumnCount - 1; c++)
+            {
+                double rand = randomNumGenerator.Next(-10, 10);
+                weights.Add(rand / 10);
+            }
+
+            //draw the intial classification boundary
+            drawLine(weights[0], weights[1]);
+            lbl_w1.Text = weights[0].ToString();
+            lbl_w2.Text = weights[1].ToString();
+            double[] weightsdata = { weights[0], weights[1] };
+            weightsHistory.Add(weightsdata);
+
+            // loop the code for the desired itteration count
+            for (int i = 0; i < numItteration; i++)
+            {
+
+                // for each instance (row) itterate and adjust the weight
+                for (int j = 0; j < dataRowCount; j++)
+                {
+                    // calculate output
+                    double output = 0;
+                    for (int c = 0; c < dataColumnCount - 1; c++)
+                    {
+                        output += weights[c] * data[j][c];
+
+                    }
+
+
+                    if (output > theta) output = 1;
+                    else output = 0;
+
+                    //calculate error
+                    double instancError = (data[j][dataColumnCount - 1] - output);
+
+
+                    // calculate delta and update weight
+                    for (int c = 0; c < dataColumnCount - 1; c++)
+                    {
+                        weights[c] = weights[c] + learningRate * (instancError * data[j][c]);
+                    }
+
+                    double[] weightsdata2 = { weights[0], weights[1] };
+                    weightsHistory.Add(weightsdata2);
+                    slope.Add(weights[0]);
+                    bias.Add(weights[1]);
+                    errorValues.Add(Math.Abs(instancError));
+                }
+            }
+
+            // save values to text
+            using (TextWriter tw = new StreamWriter("errorList.txt"))
+            {
+                foreach (double s in errorValues)
+                    tw.WriteLine(s);
+            }
+
+
+            btn_browse.Enabled = true;
+            btn_start.Enabled = true;
+        }
+
+        public void drawLine(double m, double b)
+        {
+
+            List<double> x = new List<double>();
+            List<double> y = new List<double>();
+
+            for (double i = -1; i < 4;)
+            {
+                x.Add(i);
+                //x1*w1 + x2*w2 > etta
+                y.Add(theta / b - m / b * i);
+                i += 0.1;
+            }
+
+            try
+            {
+                mychart.Series["Series2"].Points.DataBindXY(x, y);
+            }
+            catch (Exception e) { }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            isChartUpdateEnabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                txtfilelocation.Text = openFileDialog1.FileName;
+                data = readFile(openFileDialog1.FileName);
+                instantiateChart();
+                deltaRule(data);
+                isChartUpdateEnabled = false;
+                chart_index = 0;
+            }
+        }
+
+        public void readParameters()
+        {
+
+            using (var reader = new StreamReader("parameters.csv"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+                    switch (values[0])
+                    {
+                        case "theta": theta = double.Parse(values[1]); break;
+                        case "numItteration": numItteration = int.Parse(values[1]); break;
+                        case "learningRate": learningRate = double.Parse(values[1]); break;
+                    }
+                }
+            }
+            displayParameters();
+        }
+
+        public void displayParameters()
+        {
+            lbl_iteration.Text = numItteration + "";
+            lbl_theta.Text = theta + "";
+            lbl_lr.Text = learningRate + "";
+
+        }
+    }
+}
